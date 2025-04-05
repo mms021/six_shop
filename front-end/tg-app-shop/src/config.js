3dfddc698a9d634028ab4122ca5c29b67318a9ad
Maxim <makeev.21@mail.ru>
2025-04-05 20:38:44 +0300

    12

--- front-end/tg-app-shop/src/config.js
+++ front-end/tg-app-shop/src/config.js
@@ -1,6 +1,9 @@
-export const API_BASE_URL = process.env.VITE_STATIC_URL;
-export const STATIC_URL = process.env.VITE_STATIC_URL;
+import.meta.env = import.meta.env || {};
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:7770';
+export const STATIC_URL = import.meta.env.VITE_STATIC_URL || 'http://localhost:7770';
 
 export default {
-    API_BASE_URL
+    API_BASE_URL,
+    STATIC_URL
 };
--- front-end/tg-app-shop/vite.config.js
+++ front-end/tg-app-shop/vite.config.js
@@ -1,36 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [
-      vue(),
-      VueDevTools(),
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3478,
+    allowedHosts: [ 
+      'localhost',
+      '*.twc1.net',
+      'all'
     ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      }
-    },
-    server: {
-      host: '0.0.0.0',
-      port: 3478,
-      allowedHosts: [ 
-        'localhost',
-        '*.twc1.net',
-        'all'
-      ],
-      proxy: {
-        '/static': {
-          target: env.VITE_API_BASE_URL || 'http://localhost:7770',
-          changeOrigin: true,
-        }
+    proxy: {
+      '/static': {
+        target: 'http://localhost:7770',
+        changeOrigin: true,
       }
     }
   }
