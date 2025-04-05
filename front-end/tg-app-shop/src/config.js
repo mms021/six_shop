65ab0435c61065b02dfc2dd8ed1d25dcae9a7d1a
Maxim <makeev.21@mail.ru>
2025-04-05 20:58:41 +0300

    12

--- front-end/tg-app-shop/src/config.js
+++ front-end/tg-app-shop/src/config.js
@@ -1,7 +1,7 @@
-import.meta.env = import.meta.env || {};
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:7770';
-export const STATIC_URL = import.meta.env.VITE_STATIC_URL || 'http://localhost:7770';
+
+export const API_BASE_URL = 'http://localhost:7770';
+export const STATIC_URL =  'http://localhost:7770';
 
 export default {
     API_BASE_URL,
--- front-end/tg-app-shop/vite.config.js
+++ front-end/tg-app-shop/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( { 
   plugins: [
     vue(),
     VueDevTools(),
