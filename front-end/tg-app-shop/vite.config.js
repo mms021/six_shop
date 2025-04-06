b01a49d0d9dba24a1c2625e38931a7818f2aea77
Maxim <makeev.21@mail.ru>
2025-04-06 14:31:45 +0300

    12

--- front-end/tg-app-shop/vite.config.js
+++ front-end/tg-app-shop/vite.config.js
@@ -1,33 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig( { 
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3478,
-    allowedHosts: [ 
-      'localhost',
-      '*.twc1.net',
-      'all',
-      'mms021-six-shop-f408.twc1.net'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
     ],
-    proxy: {
-      '/static': {
-        target: 'http://localhost:7770',
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3478,
+      allowedHosts: [ 
+        'localhost',
+        '*.twc1.net',
+        'all',
+        'mms021-six-shop-f408.twc1.net',
+      ],
+      proxy: {
+        '/static': {
+          target:  'http://localhost:7770',
+          changeOrigin: true,
+        }
       }
     }
   }
